4a5cec951c3db5ddb3b1396bcd529af0cfdc2cf8
Daniel Baker <daniel@thrivefantasy.com>
2024-05-01 14:50:50 -0400

    changed categories

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,29 +1,56 @@
 export const CONNECTION_GAMES = [
   [
     {
-      category: "NBA Slam Dunk Contest Winners",
-      words: ["Michael Jordan",	"Vince Carter",	"Dominique Wilkins",	"Zach LaVine"],
+      category: "Athletes wit #10 Jersey",
+      words: ["Lionel Messi",	"Ben Simmons",	"Chipper Jones",	"Eli Manning"],
       difficulty: 1,
       // imageSrc: "https://i.ibb.co/Gn36Ypb/Connections-1.png",
     },
     {
-      category: "Basketball Players Turned Analysts",
-      words: ["Charles Barkley", "Shaquille O'Neal", "Kenny Smith", "Jalen Rose"],
+      category: "Athletes That Competed on Survivor",
+      words: ["Jeff Kent", "Brad Culpepper", "Cliff Robinson", "Scot Pollard"],
       difficulty: 2,
       // imageSrc: "https://i.ibb.co/3B44zXX/Connections-2.png",
-    },
+    },    																		
     {
-      category: "Athletes with #27 Jersey",
-      words: ["Rudy Gobert", "Giancarlo Stanton", "Jamal Murray", "Mike Trout"],
+      category: "Only Played For One Team In Career",
+      words: ["Dirk Nowitzki", "Kobe Bryant", "Derek Jeter", "Udonis Haslem"],
       difficulty: 3,
       // imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
     },
     {
-      category: "Featured on a Madden Cover",
-      words: ["Josh Allen", "Lamar Jackson", "Antonio Brown", "Brandon Jacobs"],
+      category: "Dual-Sport Professional Athletes",
+      words: ["Bo Jackson", "Deion Sanders", "Ellyse Perry", "Clara Hughes"],
       difficulty: 4,
       // imageSrc:
         // "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
     },
   ]
+  // [
+  //   {
+  //     category: "NBA Slam Dunk Contest Winners",
+  //     words: ["Michael Jordan",	"Vince Carter",	"Dominique Wilkins",	"Zach LaVine"],
+  //     difficulty: 1,
+  //     // imageSrc: "https://i.ibb.co/Gn36Ypb/Connections-1.png",
+  //   },
+  //   {
+  //     category: "Basketball Players Turned Analysts",
+  //     words: ["Charles Barkley", "Shaquille O'Neal", "Kenny Smith", "Jalen Rose"],
+  //     difficulty: 2,
+  //     // imageSrc: "https://i.ibb.co/3B44zXX/Connections-2.png",
+  //   },
+  //   {
+  //     category: "Athletes with #27 Jersey",
+  //     words: ["Rudy Gobert", "Giancarlo Stanton", "Jamal Murray", "Mike Trout"],
+  //     difficulty: 3,
+  //     // imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
+  //   },
+  //   {
+  //     category: "Featured on a Madden Cover",
+  //     words: ["Josh Allen", "Lamar Jackson", "Antonio Brown", "Brandon Jacobs"],
+  //     difficulty: 4,
+  //     // imageSrc:
+  //       // "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
+  //   },
+  // ]
 ];
